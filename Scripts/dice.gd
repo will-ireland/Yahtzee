36150f1f955e47fbde1ae01f8004849103564887
will-ireland <will_ireland@outlook.com>
2025-06-23 04:07:26 +1000

    added toggle buttons to the dice and a roll button

---
+++ Scripts/dice.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var roll_button : Button
+
+var die1
+var die2
+var die3
+var die4
+var die5
+
+func _ready():
+	roll_button = find_child("RollButton")
+
+func _on_roll_button_pressed():
+	pass # Replace with function body.
